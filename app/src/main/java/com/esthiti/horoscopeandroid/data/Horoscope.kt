6174f9ed58876fcb502c0541a831b22fb20a7723
Carlos Hidalgo <carlos.hidalgoh@hotmail.com>
2025-03-20 19:57:16 +0100

    Desarrollo de la vista

---
+++ app/src/main/java/com/esthiti/horoscopeandroid/data/Horoscope.kt
@@ -0,0 +1,9 @@
+package com.esthiti.horoscopeandroid.data
+
+class Horoscope (
+    val id : String, // Identificador del horóscopo
+    val name : Int, // El título del horóscopo
+    val dates : Int, // Las fechas que comprenden el horóscopo
+    val icon : Int // identificador del icono
+) {
+}
